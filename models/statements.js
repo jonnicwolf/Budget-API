6f915c1aa9142d3bf24b9531c5456767f2a3fadf
Jon Narine <jonnicwolf@gmail.com>
2021-06-16 13:30:02 -0400

    test data added

---
+++ models/statements.js
@@ -0,0 +1,25 @@
+const statements = [
+  {
+    date: Date.now(),
+    name: "Poshmark",
+    amount: 100,
+    from: "",
+  },
+  {
+    date: Date.now(),
+    name: "Apple",
+    amount: 220,
+    from: "",
+  },
+];
+// const accountTotal =()=> {
+//     sum = 0;
+//     for (let i = 0; i < statements.length; i++) {
+//       sum += statements[i].amount;
+//     }
+//     return sum;
+// }
+
+module.exports = statements
+
+
